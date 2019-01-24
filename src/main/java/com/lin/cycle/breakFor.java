87879165fa57b5dc6e6e9cd30cf4dc32f88594d5
LinFei <848531242@qq.com>
2019-01-24 17:48:01 +0800

    company computer

---
+++ src/main/java/com/lin/cycle/breakFor.java
@@ -0,0 +1,19 @@
+package com.lin.cycle;
+
+/**
+ *  跳出外层循环
+ */
+public class breakFor {
+    public static void main(String[] args) {
+
+        ok:for (int i = 0; i < 5; i++){//外层循环被标记为a,break结束的就是外层循环
+
+            for (int j = 0; j < 5; j++){
+                if (i == 1){
+                    break ok;
+                }
+                System.out.println("i: " + i + " ,j: " + j);
+            }
+        }
+    }
+}
