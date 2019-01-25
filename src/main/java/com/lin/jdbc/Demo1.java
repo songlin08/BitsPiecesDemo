82fa2a0efeb0fd918842ae55ebb149f6d362074a
LinFei <848531242@qq.com>
2019-01-25 18:07:02 +0800

    company computer

---
+++ src/main/java/com/lin/jdbc/Demo1.java
@@ -0,0 +1,33 @@
+package com.lin.jdbc;
+
+import java.sql.*;
+
+/**
+ *    第一种用Statement对象，这样不安全，容易sql注入
+ */
+public class Demo1 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, InterruptedException {
+        // 1.加载驱动
+        Class.forName("com.mysql.jdbc.Driver");
+        // 2.获得连接
+        String url = "jdbc:mysql://127.0.0.1:3306/test";
+        String username = "root";
+        String password = "123456";
+        Connection conn = DriverManager.getConnection(url,username,password);
+        // 3.获得执行对象
+        Statement statement = conn.createStatement();
+        // 4.返回结果集
+        String sql = "select * from good"; //如果要传参的话，只能用字符串拼接拼在sql字符串中。
+        ResultSet res = statement.executeQuery(sql);
+        while (res.next()){ //判断有没有下一条数据,没有返回false
+            System.out.print(res.getInt(1));
+            System.out.print("\t" + res.getString(2));
+            System.out.print("\t" + res.getInt(3));
+            System.out.println();
+        }
+        // 5.关闭资源
+        res.close();
+        statement.close();
+        conn.close();
+    }
+}
---
+++ src/main/java/com/lin/jdbc/Demo2.java
@@ -0,0 +1,35 @@
+package com.lin.jdbc;
+
+import sun.security.jca.GetInstance;
+
+import java.sql.*;
+
+public class Demo2 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        // 1.加载驱动
+        Class.forName("com.mysql.jdbc.Driver");
+        // 2.获得连接
+        String url = "jdbc:mysql://127.0.0.1:3306/test";
+        String username = "root";
+        String password = "123456";
+        Connection conn = DriverManager.getConnection(url,username,password);
+        // 3.获得预编译对象
+        String sql = "select * from good where good_count = ?"; // ?是占位符
+        PreparedStatement ps = conn.prepareStatement(sql);
+        // 4.执行sql语句
+        ps.setInt(1,100); //这是是传int型的参数，如果是String 就用setString....
+        ResultSet res = ps.executeQuery(); //执行sql语句
+        while (res.next()){
+            System.out.print(res.getInt("good_id"));
+            System.out.print("\t" + res.getString("good_name"));
+            System.out.print("\t" + res.getInt("good_count"));
+            System.out.println();
+        }
+        // 5.关闭资源
+        res.close();
+        ps.close();
+        conn.close();
+
+
+    }
+}
