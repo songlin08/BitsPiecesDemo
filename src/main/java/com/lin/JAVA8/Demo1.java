1d278b263ca79514385428f270a97201a064c1c2
LinFei <848531242@qq.com>
2019-03-05 14:31:11 +0800

    company computer

---
+++ src/main/java/com/lin/JAVA8/Demo1.java
@@ -0,0 +1,20 @@
+package com.lin.JAVA8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo1 {
+
+    public static void main(String[] args) {
+        List names = new ArrayList();
+
+        names.add("Google");
+        names.add("Runoob");
+        names.add("Taobao");
+        names.add("Baidu");
+        names.add("Sina");
+
+        names.forEach(System.out::println);
+    }
+
+}
