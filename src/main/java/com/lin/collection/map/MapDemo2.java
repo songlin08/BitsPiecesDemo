c1d9fdd0e618b2c261a79644f1cf2e4f41f8a692
LinFei <848531242@qq.com>
2019-08-06 10:07:06 +0800

    company computer

---
+++ src/main/java/com/lin/collection/map/MapDemo2.java
@@ -0,0 +1,49 @@
+package com.lin.collection.map;
+
+import com.alibaba.fastjson.JSON;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MapDemo2 {
+
+    public static void main(String[] args) {
+
+        List<Map<String,Object>> list = packageList();
+
+        for (int i = 0; i < list.size(); i++) {
+            Map<String,Object> paramMap = list.get(i);
+            System.out.println("循环list: " + JSON.toJSONString(paramMap));
+            String memberType = (String) paramMap.get("memberType");
+            switch (memberType){
+                case "00" : paramMap.put("memberTypeName","初级会员");
+                case "01" : paramMap.put("memberTypeName","中级会员");
+            }
+        }
+
+        System.out.println("最终的list: " + JSON.toJSONString(list));
+
+
+    }
+
+    public static List<Map<String,Object>> packageList(){
+        List<Map<String,Object>> list = new ArrayList<>();
+        Map<String,Object> dataMap = new HashMap<>();
+        dataMap.put("memberType","00");
+        dataMap.put("memberLevel","00");
+        dataMap.put("nsrsbh","abcd112233");
+        dataMap.put("nsrmc","四川世纪中税");
+        list.add(dataMap);
+        // 必须重新new一个对象，否则操作的是同一个对象，对map赋值会改变list中已存放的数据，因为指向的是同一个地址
+        dataMap = new HashMap<>();
+        dataMap.put("memberType","01");
+        dataMap.put("memberLevel","01");
+        dataMap.put("nsrsbh","efgh112233");
+        dataMap.put("nsrmc","四川橘子智税");
+        list.add(dataMap);
+        return list;
+    }
+
+}
