c771bbfacaed6be6873499df49fb0ce215a790b7
林非 <15608030685@163.com>
2020-05-22 11:32:22 +0800

    master分支提交代码

---
+++ src/main/java/com/lin/Master.java
@@ -0,0 +1,13 @@
+package com.lin;
+
+/**
+ * Company: 上海数慧系统技术有限公司
+ * Department: 数据中心
+ * Date: 2020/5/22 11:31
+ * Author: linf
+ * Desc：TODO
+ */
+public class Master {
+
+    //  master branch
+}
